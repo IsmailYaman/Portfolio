6fffe884f530a875fdd5fc1ba0bdd23cc469beb2
IsmailYaman <iso_yaman@outlook.com>
2023-10-20 13:36:03 +0200

    New cards design

--- app/components/projects/projectModal.tsx
+++ app/components/projects/projectModal.tsx
@@ -1,52 +1,42 @@
+// ProjectModal.tsx
+
 import Image from "next/image";
 import React from "react";
-import { Project } from "../../types";
 
-interface ModalProps {
+interface ProjectModalProps {
+    project: {
+        title: string;
+        description: { body1: string; body2?: string }[];
+        image: string;
+        links: { label: string; url: string }[];
+    };
     onClose: () => void;
 }
 
-const ProjectModal: React.FC<ModalProps & { project: Project }> = ({ onClose, project }) => {
+const ProjectModal: React.FC<ProjectModalProps> = ({ project, onClose }) => {
     return (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-            <div
-                className="absolute inset-0 bg-black opacity-50"
-
-            ></div>
-            <div className="card lg:card-side glass max-w-7xl shadow-xl">
-                <figure>
-                    <Image src={project.image} quality={100} width={500} height={500} alt={project.title} />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title text-white">{project.title}</h2>
-                    {project.description.map((desc, index) => (
-                        <>
-                            <p className="text-white text-justify" key={index}>
-                                {desc.body1}
-                            </p>
-                            <br />
-                            <p className="text-white text-justify" key={index}>
-                                {desc.body2}
-                            </p>
-                        </>
-                    ))}
-                    <p className="text-white font-bold ">I&apos;ve worked on this project using:</p>
-                    <div className="card-actions justify-start">
-                        {project.tools.map((tool) => (
-                            <div key={tool.label} className="badge badge-primary text-white">
-                                {tool.label}
-                            </div>
-                        ))}
-                    </div>
-
-                    {/* <div className="card-actions justify-end">
-                        <button className="btn btn-primary" onClick={onClose}>
-                            Close
-                        </button>
-                    </div> */}
-                </div>
+        <dialog id="projectModal" className="modal bg-black bg-opacity-50 backdrop-blur-sm  ease-in-out" open>
+            <div className="modal-box">
+                <form method="dialog">
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={onClose}>
+                        ✕
+                    </button>
+                </form>
+                <Image src={project.image} quality={100} alt={project.title} width={500} height={500} />
+                <h2>{project.title}</h2>
+                {project.description.map((desc, index) => (
+                    <p key={index}>
+                        {desc.body1} <br /> <br /> {desc.body2}
+                    </p>
+                ))}
+                {project.links.map((link, index) => (
+                    <a key={index} href={link.url} target="_blank" rel="noopener noreferrer" className="text-blue-600">
+                        {link.label}
+                    </a>
+                ))}
             </div>
-        </div>
+        </dialog>
     );
 };
+
 export default ProjectModal;
--- app/components/projects/projects.tsx
+++ app/components/projects/projects.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import ProjectCard from './projectCard';
-import { projects } from '../../data/data'; // Import the projects data
+import ProjectModal from './projectModal'; // Import the ProjectModal component
+import { projects } from '../../data/data';
 
 const Projects = () => {
+  const [selectedProject, setSelectedProject] = useState<number | null>(null);
+
+  const openModal = (projectId: number) => {
+    setSelectedProject(projectId);
+  };
+
+  const closeModal = () => {
+    setSelectedProject(null);
+  };
+
   return (
     <div className="py-5 text-base-100 bg-white">
       <div className="sm:container mx-auto">
@@ -10,11 +22,20 @@ const Projects = () => {
         <div className="flex flex-wrap mx-4">
           {projects.map((project) => (
             <div key={project.id} className="w-full sm:w-1/2 md:w-1/3 p-4">
-              <ProjectCard project={project} /> {/* Pass the project data as prop */}
+              <div onClick={() => openModal(project.id)} className="cursor-pointer">
+                <ProjectCard project={project} />
+              </div>
             </div>
           ))}
         </div>
       </div>
+
+      {selectedProject !== null && (
+        <ProjectModal
+          project={projects[selectedProject - 1]}
+          onClose={closeModal}
+        />
+      )}
     </div>
   );
 };
