82b904d783b187dbb3923e1ffebdc79d8c2f220d
IsmailYaman <iso_yaman@outlook.com>
2023-10-23 16:54:35 +0200

    - Changed project modal - Changed button in banner

--- app/components/projects/projectModal.tsx
+++ app/components/projects/projectModal.tsx
@@ -1,5 +1,3 @@
-// ProjectModal.tsx
-
 import Image from "next/image";
 import React from "react";
 
@@ -9,33 +7,63 @@ interface ProjectModalProps {
         description: { body1: string; body2?: string }[];
         image: string;
         links: { label: string; url: string }[];
+        tools: { label: string; image: string }[];
     };
     onClose: () => void;
 }
 
 const ProjectModal: React.FC<ProjectModalProps> = ({ project, onClose }) => {
     return (
-        <dialog id="projectModal" className="modal bg-black bg-opacity-50 backdrop-blur-sm  ease-in-out" open>
-            <div className="modal-box">
-                <form method="dialog">
-                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={onClose}>
-                        ✕
-                    </button>
-                </form>
-                <Image src={project.image} quality={100} alt={project.title} width={500} height={500} />
-                <h2>{project.title}</h2>
-                {project.description.map((desc, index) => (
-                    <p key={index}>
-                        {desc.body1} <br /> <br /> {desc.body2}
-                    </p>
-                ))}
-                {project.links.map((link, index) => (
-                    <a key={index} href={link.url} target="_blank" rel="noopener noreferrer" className="text-blue-600">
-                        {link.label}
-                    </a>
-                ))}
+        <dialog id="projectModal" className="modal bg-black bg-opacity-50 backdrop-blur-sm ease-in-out" open>
+    <div className="modal-box flex w-11/12 max-w-5xl">
+        <form method="dialog">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={onClose}>
+                ✕
+            </button>
+        </form>
+
+        <div className="flex-shrink-0">
+            <Image src={project.image} quality={100} alt={project.title} width={500} height={500} />
+        </div>
+
+        <div className="ml-5 flex-grow">
+            <h2>{project.title}</h2>
+            {project.description.map((desc, index) => (
+                <div key={index} className="mb-5 justifty-between">
+                    <p className="mb-4">{desc.body1}</p>
+                    {desc.body2 && <p className="mb-4">{desc.body2}</p>}
+                </div>
+            ))}
+
+            <div className="flex w-full justify-between">
+                <div className="flex flex-col">
+                    <strong className="mb-1">Tools used:</strong>
+                    <div className="flex flex-wrap">
+                        {project.tools.map((tool, index) => (
+                            <div key={index} className="badge badge-primary badge-sm mr-1 mb-1 text-white">
+                                {tool.label}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="flex flex-col align-bottom self-end">
+                    {project.links.map((link, index) => (
+                        <a
+                            key={index}
+                            href={link.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="btn btn-primary"
+                        >
+                            {link.label}
+                        </a>
+                    ))}
+                </div>
             </div>
-        </dialog>
+        </div>
+    </div>
+</dialog>
+
     );
 };
 
--- app/components/projects/projects.tsx
+++ app/components/projects/projects.tsx
@@ -1,43 +1,38 @@
-'use client'
-import React, { useState } from 'react';
-import ProjectCard from './projectCard';
-import ProjectModal from './projectModal'; // Import the ProjectModal component
-import { projects } from '../../data/data';
+"use client";
+import React, { useState } from "react";
+import ProjectCard from "./projectCard";
+import ProjectModal from "./projectModal"; // Import the ProjectModal component
+import { projects } from "../../data/data";
 
 const Projects = () => {
-  const [selectedProject, setSelectedProject] = useState<number | null>(null);
+    const [selectedProject, setSelectedProject] = useState<number | null>(null);
 
-  const openModal = (projectId: number) => {
-    setSelectedProject(projectId);
-  };
+    const openModal = (projectId: number) => {
+        setSelectedProject(projectId);
+    };
 
-  const closeModal = () => {
-    setSelectedProject(null);
-  };
+    const closeModal = () => {
+        setSelectedProject(null);
+    };
 
-  return (
-    <div className="py-5 text-base-100 bg-white">
-      <div className="sm:container mx-auto">
-        <h1 className="text-center">Projects</h1>
-        <div className="flex flex-wrap mx-4">
-          {projects.map((project) => (
-            <div key={project.id} className="w-full sm:w-1/2 md:w-1/3 p-4">
-              <div onClick={() => openModal(project.id)} className="cursor-pointer">
-                <ProjectCard project={project} />
-              </div>
+    return (
+        <div className="py-5 text-base-100 bg-white">
+            <div className="sm:container mx-auto">
+                <h1 className="text-center">Projects</h1>
+                <div className="flex flex-wrap mx-4">
+                    {projects.map((project) => (
+                        <div key={project.id} className="w-full sm:w-1/2 md:w-1/3 p-4">
+                            <div onClick={() => openModal(project.id)} className="cursor-pointer">
+                                <ProjectCard project={project} />
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))}
-        </div>
-      </div>
 
-      {selectedProject !== null && (
-        <ProjectModal
-          project={projects[selectedProject - 1]}
-          onClose={closeModal}
-        />
-      )}
-    </div>
-  );
+            {selectedProject !== null && <ProjectModal project={projects[selectedProject - 1]} onClose={closeModal} />}
+        </div>
+    );
 };
 
 export default Projects;
